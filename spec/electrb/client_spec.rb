3c05bfd3e6d13e467db9331c085079f7af260a30
azuchi <azuchi@chaintope.com>
2019-07-05 14:33:36 +0900

    Fix testcase

--- spec/electrb/client_spec.rb
+++ spec/electrb/client_spec.rb
@@ -9,7 +9,7 @@ RSpec.describe Electrb::Client do
       txid = 'd513c8fa051336fc8059958a7c99dc390c741a693503b67783392d0a84029bcb'
       allow(client).to receive(:client).and_return(c)
       allow(c).to receive('blockchain.transaction.get'.to_sym).and_return({'result': ""})
-      expect(c).to receive('blockchain.transaction.get'.to_sym).with([txid]).once
+      expect(c).to receive('blockchain.transaction.get'.to_sym).with(txid).once
       client.blockchain_transaction_get(txid)
     end
   end
